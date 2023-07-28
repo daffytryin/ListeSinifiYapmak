2548f60a486c91c11cb1006b83d6678ea410390e
daffytryin <sumeyye-saban@hotmail.com>
2023-07-28 04:06:15 +0300

    .

--- src/MyList.java
+++ src/MyList.java
@@ -1,33 +1,30 @@
-public class MyList<T> {
+import java.util.Arrays;
 
-    private Object[] array; // Array to hold the data
-    private int size;  // Current number of elements in the array
-    private static final int CAPACITY = 10; //Max capacity given
+public class MyList<T> {
+    private Object[] array;
+    private int size;
+    private static final int DEFAULT_CAPACITY = 10;
 
     public MyList() {
-        this(CAPACITY);
+        this(DEFAULT_CAPACITY);
     }
 
-    // Constructor with custom initial capacity
     public MyList(int capacity) {
         if (capacity <= 0) {
-            System.out.println("Capacity must be bigger than zero!");
+            throw new IllegalArgumentException("Capacity must be greater than 0");
         }
         array = new Object[capacity];
         size = 0;
     }
 
-    // Returns the current number of elements in the list
     public int size() {
         return size;
     }
 
-    // Returns the current capacity of the array
     public int getCapacity() {
         return array.length;
     }
 
-    // Adds an element to the list
     public void add(T data) {
         if (size == array.length) {
             increaseCapacity();
@@ -35,11 +32,99 @@ public class MyList<T> {
         array[size++] = data;
     }
 
-    // Increases the capacity of the array by doubling its size
+    public T get(int index) {
+        if (index < 0 || index >= size) {
+            throw new IndexOutOfBoundsException("Index out of range");
+        }
+        return (T) array[index];
+    }
+
     private void increaseCapacity() {
         int newCapacity = array.length * 2;
         Object[] newArray = new Object[newCapacity];
         System.arraycopy(array, 0, newArray, 0, size);
         array = newArray;
+    }
+
+    public void remove(int index) {
+        if (index < 0 || index >= size) {
+            throw new IndexOutOfBoundsException("Index out of range");
+        }
+
+        // Shift elements to the left to remove the element at the given index
+        for (int i = index; i < size - 1; i++) {
+            array[i] = array[i + 1];
+        }
+        array[size - 1] = null;
+        size--;
+    }
+
+    public void set(int index, T data) {
+        if (index < 0 || index >= size) {
+            throw new IndexOutOfBoundsException("Index out of range");
+        }
+        array[index] = data;
+    }
+
+    public int indexOf(T data) {
+        for (int i = 0; i < size; i++) {
+            if (array[i].equals(data)) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public int lastIndexOf(T data) {
+        for (int i = size - 1; i >= 0; i--) {
+            if (array[i].equals(data)) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+
+    public T[] toArray() {
+        return (T[]) Arrays.copyOf(array, size);
+    }
+
+    public void clear() {
+        for (int i = 0; i < size; i++) {
+            array[i] = null;
+        }
+        size = 0;
+    }
+
+    public MyList<T> subList(int start, int finish) {
+        if (start < 0 || finish >= size || start > finish) {
+            throw new IndexOutOfBoundsException("Invalid start or finish index");
+        }
+        MyList<T> sublist = new MyList<>(finish - start + 1);
+        for (int i = start; i <= finish; i++) {
+            sublist.add(get(i));
+        }
+        return sublist;
+    }
+
+    public boolean contains(T data) {
+        return indexOf(data) != -1;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder("[");
+        for (int i = 0; i < size; i++) {
+            sb.append(array[i]);
+            if (i < size - 1) {
+                sb.append(",");
+            }
+        }
+        sb.append("]");
+        return sb.toString();
     }
 }
